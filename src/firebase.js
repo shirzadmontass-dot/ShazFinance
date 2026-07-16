26ed16514ab0e55a3b3055ead5db5b1b13cc1a9a
shirzadmontass@gmail.com <shirzadmontass@gmail.com>
2026-07-16 20:07:06 +0100

    Fix Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,9 @@
-import { initializeApp } from "firebase/app"
-import { getDatabase, ref, onValue, set } from "firebase/database"
-
 const firebaseConfig = {
   apiKey: "AIzaSyAxrjqHbJ-gvWre-a9gg3WGdQaRF3W34o0",
   authDomain: "shazplan.firebaseapp.com",
   databaseURL: "https://shazplan-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "shazplan"
+  projectId: "shazplan",
+  storageBucket: "shazplan.appspot.com",
+  messagingSenderId: "238809011616",
+  appId: "1:238809011616:web:38dc59871d132a3067c0c5"
 }
-
-const app = initializeApp(firebaseConfig)
-
-export const db = getDatabase(app)
-export const dbRef = ref(db, "store")
-
-export { onValue, set }
